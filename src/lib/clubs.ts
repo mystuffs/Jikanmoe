18165d369013e3f848d128e9106c510c73e64460
Ruzie <luhamailbox@gmail.com>
2022-05-06 10:48:37 -0700

    add clubs

--- src/lib/clubs.ts
+++ src/lib/clubs.ts
@@ -1,3 +1,29 @@
+import { IClubMembers, IClubRelations, IClubsById, IClubsSearch, IClubStaff } from "../typings/clubs";
+import fetchURL from "./util";
+
 export default class Clubs {
-    public async getClubsById(id: number): Promise<
+    public async getClubsById(id: number): Promise<IClubsById> {
+        const data = await fetchURL<IClubsById>(`/clubs/${id}`);
+        return data;
+    }
+
+    public async getClubMembers(id: number, page?: number): Promise<IClubMembers> {
+        const data = await fetchURL<IClubMembers>(page ? `/clubs/${id}/members?page=${page}` : `/clubs/${id}/members`);
+        return data;
+    }
+
+    public async getClubStaff(id: number): Promise<IClubStaff> {
+        const data = await fetchURL<IClubStaff>(`/clubs/${id}/staff`);
+        return data;
+    }
+
+    public async getClubRelations(id: number): Promise<IClubRelations> {
+        const data = await fetchURL<IClubRelations>(`/clubs/${id}/staff`);
+        return data;
+    }
+
+    public async getClubsSearch(endpoint: string): Promise<IClubsSearch> {
+        const data = await fetchURL<IClubsSearch>(`/clubs${endpoint}`);
+        return data;
+    }
 }
--- src/typings/clubs.d.ts
+++ src/typings/clubs.d.ts
@@ -14,3 +14,71 @@ export interface IClubsById {
         access: string,
     }
 }
+
+export interface IClubMembers {
+    pagination: {
+        last_visible_page: 0,
+        has_next_page: true,
+    },
+    data: {
+        jpg: {
+            image_url: string,
+        },
+        webp: {
+            image_url: string,
+        },
+    }[]
+}
+
+export interface IClubStaff {
+    data: {
+        url: string,
+        username: string,
+    }[],
+}
+
+export interface IClubRelations {
+    data: {
+        anime: {
+            mal_id: 0,
+            type: string,
+            name: string,
+            url: string,
+        }[],
+        manga: {
+            mal_id: 0,
+            type: string,
+            name: string,
+            url: string,
+        }[],
+        characters: {
+            mal_id: 0,
+            type: string,
+            name: string,
+            url: string
+        }[],
+    }
+}
+
+export interface IClubsSearch {
+    data: {
+        data: {
+            mal_id: number,
+            name: string,
+            url: string,
+            images: {
+                jpg: {
+                    image_url: string,
+                },
+            },
+            members: number,
+            category: string,
+            created: string,
+            access: string,
+        }[],
+        pagination: {
+            last_visible_page: number,
+            has_next_page: boolean,
+        },
+    }
+}
