0d140755adbe2d2f04982ca6701bfb34fea82cf1
Ruzie <luhamailbox@gmail.com>
2022-05-07 00:45:17 -0700

    completed manga

--- src/lib/manga.ts
+++ src/lib/manga.ts
@@ -1,11 +1,23 @@
 import type {
     IMangaById,
     IMangaCharacters,
+    IMangaExternal,
+    IMangaMoreInfo,
     IMangaNews,
+    IMangaPictures,
+    IMangaRecommendations,
+    IMangaRelations,
+    IMangaReviews,
+    IMangaSearch,
+    IMangaStatistics,
     IMangaTopics,
+    IMangaUserUpdates,
 } from "../typings/manga";
 import fetchURL from "./util";
 
+/**
+ * @todo continue from https://docs.api.jikan.moe/#operation/getMangaTopics
+ */
 export default class Manga {
     public async getMangaById(id: number): Promise<IMangaById> {
         const data = await fetchURL<IMangaById>(`/manga/${id}`);
@@ -31,6 +43,56 @@ export default class Manga {
             data = await fetchURL<IMangaTopics>(`/manga/${id}/forum?filter=${filter}`);
         } else {
             data = await fetchURL<IMangaTopics>(`/manga/${id}/forum`);
+        }
+        return data;
+    }
+
+    public async getMangaPictures(id: number): Promise<IMangaPictures> {
+        const data = await fetchURL<IMangaPictures>(`/manga/${id}/pictures`);
+        return data;
+    }
+
+    public async getMangaStatistics(id: number): Promise<IMangaStatistics> {
+        const data = await fetchURL<IMangaStatistics>(`/manga/${id}/statistics`);
+        return data;
+    }
+
+    public async getMangaMoreInfo(id: number): Promise<IMangaMoreInfo> {
+        const data = await fetchURL<IMangaMoreInfo>(`/manga/${id}/moreinfo`);
+        return data;
+    }
+
+    public async getMangaRecommendations(id: number): Promise<IMangaRecommendations> {
+        const data = await fetchURL<IMangaRecommendations>(`/manga/${id}/recommendations`);
+        return data;
+    }
+
+    public async getMangaUserUpdates(id: number, page?: number): Promise<IMangaUserUpdates> {
+        const data = await fetchURL<IMangaUserUpdates>(page ? `/manga/${id}/userupdates?page=${page}` : `/manga/${id}/userupdates`);
+        return data;
+    }
+
+    public async getMangaReviews(id: number, page?: number): Promise<IMangaReviews> {
+        const data = await fetchURL<IMangaReviews>(page ? `/manga/${id}/reviews?page=${page}` : `/manga/${id}/reviews`);
+        return data;
+    }
+
+    public async getMangaRelations(id: number): Promise<IMangaRelations> {
+        const data = await fetchURL<IMangaRelations>(`/manga/${id}/relations`);
+        return data;
+    }
+
+    public async getMangaExternal(id: number): Promise<IMangaExternal> {
+        const data = await fetchURL<IMangaExternal>(`/manga/${id}/external`);
+        return data;
+    }
+
+    public async getMangaSearch(endpoint?: string): Promise<IMangaSearch> {
+        let data;
+        if (endpoint) {
+            data = await fetchURL<IMangaSearch>(`/manga${endpoint}`);
+        } else {
+            data = await fetchURL<IMangaSearch>("/manga");
         }
         return data;
     }
--- src/typings/manga.d.ts
+++ src/typings/manga.d.ts
@@ -137,7 +137,7 @@ export interface IMangaTopics {
         date: string,
         author_username: string,
         author_url: string,
-        comments: 0,
+        comments: number,
         last_comment: {
             url: string,
             author_username: string,
@@ -145,4 +145,243 @@ export interface IMangaTopics {
             date: string,
         },
     }[],
+}
+
+export interface IMangaPictures {
+    data: {
+        jpg: {
+            image_url: string,
+            small_image_url: string,
+            large_image_url: string,
+        },
+        webp: {
+            image_url: string,
+            small_image_url: string,
+            large_image_url: string,
+        },
+    }[]
+}
+
+export interface IMangaStatistics {
+    data: {
+        reading: number,
+        completed: number,
+        on_hold: number,
+        dropped: number,
+        plan_to_read: number,
+        total: number,
+        scores: {
+            score: number,
+            votes: number,
+            percentage: number,
+        }[],
+    }
+}
+
+export interface IMangaMoreInfo {
+    data: {
+        moreinfo: string,
+    }
+}
+
+export interface IMangaRecommendations {
+    data: {
+        entry: {
+            mal_id: number,
+            url: string,
+            images: {
+                jpg: {
+                    image_url: string,
+                    small_image_url: string,
+                    large_image_url: string,
+                },
+                webp: {
+                    image_url: string,
+                    small_image_url: string,
+                    large_image_url: string,
+                },
+            },
+            title: string,
+        },
+    }[]
+}
+
+export interface IMangaUserUpdates {
+    data: {
+        user: {
+            username: string,
+            url: string,
+            images: {
+                jpg: {
+                    image_url: string,
+                },
+                webp: {
+                    image_url: string,
+                },
+            },
+        },
+        score: number,
+        status: string,
+        volumes_read: number,
+        volumes_total: number,
+        chapters_read: number,
+        chapters_total: number,
+        date: string,
+    }[],
+    pagination: {
+        last_visible_page: number,
+        has_next_page: boolean,
+    }
+}
+
+export interface IMangaReviews {
+    data: {
+        user: {
+            username: string,
+            url: string,
+            images: {
+                jpg: {
+                    image_url: string,
+                },
+                webp: {
+                    image_url: string,
+                },
+            },
+        },
+        mal_id: number,
+        url: string,
+        type: string,
+        votes: number,
+        date: string,
+        chapters_read: number,
+        review: string,
+        scores: {
+            overall: number,
+            story: number,
+            art: number,
+            character: number,
+            enjoyment: number,
+        }
+    }[],
+    pagination: {
+        last_visible_page: number,
+        has_next_page: boolean,
+    }
+}
+
+export interface IMangaRelations {
+    data: {
+        relation: string,
+        entry: {
+            mal_id: number,
+            type: string,
+            name: string,
+            url: string,
+        }[],
+    }[],
+}
+
+export interface IMangaExternal {
+    data: {
+        name: string,
+        url: string,
+    }[]
+}
+
+export interface IMangaSearch {
+    data: {
+        mal_id: number,
+        url: string,
+        images: {
+            jpg: {
+                image_url: string,
+                small_image_url: string,
+                large_image_url: string,
+            },
+            webp: {
+                image_url: string,
+                small_image_url: string,
+                large_image_url: string,
+            },
+        },
+        title: string,
+        title_english: string,
+        title_japanese: string,
+        title_synonyms: string[],
+        type: string,
+        chapters: number,
+        volumes: number,
+        status: string,
+        publishing: boolean,
+        published: {
+            from: string,
+            to: string,
+            prop: {
+                from: {
+                    day: number,
+                    month: number,
+                    year: number,
+                },
+                to: {
+                    day: number,
+                    month: number,
+                    year: number,
+                },
+                string: string,
+            }
+        },
+        score: number,
+        scored_by: number,
+        rank: number,
+        popularity: number,
+        members: number,
+        favorites: number,
+        synopsis: string,
+        background: string,
+        authors: {
+            mal_id: number,
+            type: string,
+            name: string,
+            url: string,
+        }[],
+        serializations: {
+            mal_id: number,
+            type: string,
+            name: string,
+            url: string,
+        }[],
+        genres: {
+            mal_id: number,
+            type: string,
+            name: string,
+            url: string
+        }[],
+        explicit_genres: {
+            mal_id: number,
+            type: string,
+            name: string,
+            url: string
+        }[],
+        themes: {
+            mal_id: number,
+            type: string,
+            name: string,
+            url: string
+        }[],
+        demographics: {
+            mal_id: number,
+            type: string,
+            name: string,
+            url: string
+          }[],
+      }[],
+    pagination: {
+        last_visible_page: number,
+        has_next_page: boolean,
+        items: {
+            count: number,
+            total: number,
+            per_page: number,
+        }
+    }
 }
