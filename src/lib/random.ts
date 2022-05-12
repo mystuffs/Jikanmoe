5b9f3787a99d9defd829f812cae86a2b998b425e
Ruzie <luhamailbox@gmail.com>
2022-05-12 13:42:02 +0530

    added random

---
+++ src/lib/random.ts
@@ -0,0 +1,29 @@
+import { IRandomAnime, IRandomChatacters, IRandomManga, IRandomPeople, IRandomUsers } from "../typings/random";
+import fetchURL from "./util";
+
+export default class Random {
+    public async getRandomAnime(): Promise<IRandomAnime> {
+        const data = await fetchURL<IRandomAnime>("/random/anime");
+        return data;
+    }
+
+    public async getRandomManga(): Promise<IRandomManga> {
+        const data = await fetchURL<IRandomManga>("/random/manga");
+        return data;
+    }
+
+    public async getRandomCharacters(): Promise<IRandomChatacters> {
+        const data = await fetchURL<IRandomChatacters>("/random/characters");
+        return data;
+    }
+
+    public async getRandomPeople(): Promise<IRandomPeople> {
+        const data = await fetchURL<IRandomPeople>("/random/people");
+        return data;
+    }
+
+    public async getRandomUsers(): Promise<IRandomUsers> {
+        const data = await fetchURL<IRandomUsers>("/random/users");
+        return data;
+    }
+}
