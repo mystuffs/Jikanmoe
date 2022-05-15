cdb1d8aa0f784be4e421de161795d3c6d144e08b
Ruzie <luhamailbox@gmail.com>
2022-05-15 11:25:25 -0700

    added: users

--- src/lib/users.ts
+++ src/lib/users.ts
@@ -1,9 +1,77 @@
-import type { IUsersSearch } from "../typings/users";
+import type {
+    IUserAbout,
+    IUserClubs,
+    IUserFavorites,
+    IUserFriends,
+    IUserHistory,
+    IUserID,
+    IUserProfile,
+    IUserRecommendations,
+    IUserReviews,
+    IUsersSearch,
+    IUserStatistics,
+    IUserUpdates,
+} from "../typings/users";
 import fetchURL from "./util";
 
 export default class Users {
-    public async getUsersSearch(endpoint: string): Promise<IUsersSearch> {
-        const data = await fetchURL<IUsersSearch>(`/users${endpoint}`);
+    public async getUsersSearch(params?: string): Promise<IUsersSearch> {
+        const data = await fetchURL<IUsersSearch>(params ? `/users${params}` : "/users");
+        return data;
+    }
+
+    public async getUserById(): Promise<IUserID> {
+        const data = await fetchURL<IUserID>("/users/userbyid");
+        return data;
+    }
+
+    public async getUserProfile(username: string): Promise<IUserProfile> {
+        const data = await fetchURL<IUserProfile>(`/users/${username}`);
+        return data;
+    }
+
+    public async getUserStatistics(username: string): Promise<IUserStatistics> {
+        const data = await fetchURL<IUserStatistics>(`/users/${username}/statistics`);
+        return data;
+    }
+
+    public async getUserFavorites(username: string): Promise<IUserFavorites> {
+        const data = await fetchURL<IUserFavorites>(`/users/${username}/favorites`);
+        return data;
+    }
+
+    public async getUserUpdates(username: string): Promise<IUserUpdates> {
+        const data = await fetchURL<IUserUpdates>(`/users/${username}/userupdates`);
+        return data;
+    }
+
+    public async getUserAbout(username: string): Promise<IUserAbout> {
+        const data = await fetchURL<IUserAbout>(`/users/${username}/about`);
+        return data;
+    }
+
+    public async getUserHistory(username: string, params?: string): Promise<IUserHistory> {
+        const data = await fetchURL<IUserHistory>(params ? `/users/${username}/history${params}` : `/users/${username}/history`);
+        return data;
+    }
+
+    public async getUserFriends(username: string): Promise<IUserFriends> {
+        const data = await fetchURL<IUserFriends>(`/users/${username}/friends`);
+        return data;
+    }
+
+    public async getUserReviews(username: string): Promise<IUserReviews> {
+        const data = await fetchURL<IUserReviews>(`/users/${username}/friends`);
+        return data;
+    }
+
+    public async getUserRecommendations(username: string): Promise<IUserRecommendations> {
+        const data = await fetchURL<IUserRecommendations>(`/users/${username}/recommendations`);
+        return data;
+    }
+
+    public async getUserClubs(username: string): Promise<IUserClubs> {
+        const data = await fetchURL<IUserClubs>(`/users/${username}/clubs`);
         return data;
     }
 }
--- src/typings/users.d.ts
+++ src/typings/users.d.ts
@@ -5,3 +5,329 @@ export interface IUsersSearch {
         has_next_page: boolean,
     }
 }
+
+export interface IUserID {
+    data: {
+        url: string,
+        username: string,
+    }
+}
+
+export interface IUserProfile {
+    mal_id: number,
+    username: string,
+    url: string,
+    images: {
+        jpg: {
+            image_url: string,
+        },
+        webp: {
+            image_url: string,
+        },
+    },
+    last_online: string,
+    gender: string,
+    birthday: string,
+    location: string,
+    joined: string,
+}
+
+export interface IUserStatistics {
+    data: {
+        anime: {
+            days_watched: number,
+            mean_score: number,
+            watching: number,
+            completed: number,
+            on_hold: number,
+            dropped: number,
+            plan_to_watch: number,
+            total_entries: number,
+            rewatched: number,
+            episodes_watched: number,
+        },
+        manga: {
+            days_read: number,
+            mean_score: number,
+            reading: number,
+            completed: number,
+            on_hold: number,
+            dropped: number,
+            plan_to_read: number,
+            total_entries: number,
+            reread: number,
+            chapters_read: number,
+            volumes_read: number,
+        },
+    }
+}
+
+export interface IUserFavorites {
+    data: {
+        anime: {
+            type: string,
+            start_year: number,
+            mal_id: number,
+            url: string,
+            images: {
+                jpg: {
+                    image_url: string,
+                    small_image_url: string,
+                    large_image_url: string,
+                },
+                webp: {
+                    image_url: string,
+                    small_image_url: string,
+                    large_image_url: string,
+                },
+            },
+            title: string,
+        }[],
+        manga: {
+            type: string,
+            start_year: number,
+            mal_id: number,
+            url: string,
+            images: {
+                jpg: {
+                    image_url: string,
+                    small_image_url: string,
+                    large_image_url: string
+                },
+                webp: {
+                    image_url: string,
+                    small_image_url: string,
+                    large_image_url: string
+                },
+          },
+          title: string,
+        }[],
+        characters: {
+             : {
+                mal_id: number,
+                type: string,
+                title: string,
+                url: string,
+            },
+            mal_id: number,
+            url: string,
+            images: {
+                jpg: {
+                    image_url: string,
+                    small_image_url: string,
+                },
+                webp: {
+                    image_url: string,
+                    small_image_url: string,
+                },
+          },
+          name: string
+      }[],
+      people: {
+           : {
+              mal_id: number,
+              url: string,
+              images: {
+                  jpg: {
+                      image_url: string,
+                      small_image_url: string,
+                    },
+                    webp: {
+                        image_url: string,
+                        small_image_url: string,
+                    },
+                },
+                name: string,
+            },
+        }[],
+    }
+}
+
+export interface IUserUpdates {
+    data: {
+        anime: {
+            entry: {
+                mal_id: number,
+                url: string,
+                images: {
+                    jpg: {
+                        image_url: string,
+                        small_image_url: string,
+                        large_image_url: string,
+                    },
+                    webp: {
+                        image_url: string,
+                        small_image_url: string,
+                        large_image_url: string,
+                    }
+                },
+                title: string,
+            },
+            score: number,
+            status: string,
+            episodes_seen: number,
+            episodes_total: number,
+            date: string,
+        }[],
+        manga: {
+            entry: {
+                mal_id: number,
+                url: string,
+                images: {
+                    jpg: {
+                        image_url: string,
+                        small_image_url: string,
+                        large_image_url: string,
+                    },
+                    webp: {
+                        image_url: string,
+                        small_image_url: string,
+                        large_image_url: string,
+                    }
+                },
+                title: string,
+            },
+            score: number,
+            status: string,
+            chapters_read: number,
+            chapters_total: number,
+            volumes_read: number,
+            volumes_total: number,
+            date: string
+        }[],
+    }
+}
+
+export interface IUserAbout {
+    data: {
+        about: string,
+    }[]
+}
+
+export interface IUserHistory {
+    data: {}[]
+}
+
+export interface IUserFriends {
+    data: {
+        user: {
+            username: string,
+            url: string,
+            images: {
+                jpg: {
+                    image_url: string,
+                },
+                webp: {
+                    image_url: string,
+                },
+            },
+        },
+        last_online: string,
+        friends_since: string,
+    }[],
+    pagination: {
+        last_visible_page: number,
+        has_next_page: boolean,
+    }
+}
+
+export interface IUserReviews {
+    data: {
+        data: {
+            user: {
+                username: string,
+                url: string,
+                images: {
+                    jpg: {
+                        image_url: string,
+                    },
+                    webp: {
+                        image_url: string,
+                    },
+                },
+            },
+            anime: {
+                mal_id: number,
+                url: string,
+                images: {
+                    jpg: {
+                        image_url: string,
+                        small_image_url: string,
+                        large_image_url: string,
+                    },
+                    webp: {
+                        image_url: string,
+                        small_image_url: string,
+                        large_image_url: string,
+                    },
+                },
+                title: string,
+            },
+            mal_id: number,
+            url: string,
+            type: string,
+            votes: number,
+            date: string,
+            review: string,
+            episodes_watched: number,
+            scores: {
+                overall: number,
+                story: number,
+                animation: number,
+                sound: number,
+                character: number,
+                enjoyment: number
+            },
+        }[],
+        pagination: {
+            last_visible_page: number,
+            has_next_page: boolean,
+        },
+    }
+}
+
+export interface IUserRecommendations {
+    data: {
+        mal_id: string,
+        entry: {
+            mal_id: number,
+            url: string,
+            images: {
+                jpg: {
+                    image_url: string,
+                    small_image_url: string,
+                    large_image_url: string,
+                },
+                webp: {
+                    image_url: string,
+                    small_image_url: string,
+                    large_image_url: string,
+                },
+            },
+            title: string,
+        }[],
+        content: string,
+        user: {
+            data: {
+                url: string,
+                username: string,
+            },
+        },
+    }[],
+    pagination: {
+        last_visible_page: number,
+        has_next_page: true,
+    }
+}
+
+export interface IUserClubs {
+    data: {
+        mal_id: number,
+        name: string,
+        url: string,
+    }[],
+    pagination: {
+        last_visible_page: number,
+        has_next_page: boolean,
+    }
+}
