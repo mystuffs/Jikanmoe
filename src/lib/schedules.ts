cd20e73fb3d3ce06b85c9a655f7840561c9cd840
Ruzie <luhamailbox@gmail.com>
2022-05-15 10:36:14 -0700

    fix: endpoint is optional

--- src/lib/schedules.ts
+++ src/lib/schedules.ts
@@ -2,8 +2,8 @@ import fetchURL from "./util";
 import type { ISchedules } from "../typings/schedules";
 
 export default class Schedules {
-    public async getSchedules(endpoint: string): Promise<ISchedules> {
-        const data = await fetchURL<ISchedules>(`/schedules${endpoint}`);
+    public async getSchedules(endpoint?: string): Promise<ISchedules> {
+        const data = await fetchURL<ISchedules>(endpoint ? `/schedules${endpoint}` : "/schedules");
         return data;
     }
 }
